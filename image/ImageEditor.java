8a0a625528f2526d393604a04d66919e1ec76e55
archsak <aharon.saksonov@mail.huji.ac.il>
2024-02-21 21:10:20 +0200

    imageEditor2

--- image/ImageEditor.java
+++ image/ImageEditor.java
@@ -2,6 +2,9 @@ package image;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+//import java.awt.Graphics2D;
+
+
 
 
 public class ImageEditor {
@@ -17,21 +20,26 @@ public class ImageEditor {
         int widthDifference = newWidth - image.getWidth();
         int heightDifference = newHeight - image.getHeight();
 
-        Color[][] paddedImage;
+        Color[][] paddedImage = new Color[newHeight][newWidth];
+
+        // Apply white padding to the necessary areas
         for (int row = 0; row < newHeight; row++){
             for (int col = 0; col < newWidth; col++){
-                if (row < heightDifference / 2 || row >= newHeight - heightDifference / 2) {
-                    paddedImage[row][col] = Color.WHITE;
-                }
-                else if (col < widthDifference / 2 || row >= newWidth - widthDifference / 2) {
+                if (row < heightDifference / 2 ||
+                        row >= newHeight - heightDifference / 2 ||
+                        col < widthDifference / 2 ||
+                        col >= newWidth - widthDifference / 2) {
                     paddedImage[row][col] = Color.WHITE;
                 }
                 else{
                     paddedImage[row][col] =
+                            image.getPixel(row - heightDifference,
+                                    col - widthDifference);
                 }
             }
         }
-        image = new Image()
+
+        image = new Image(paddedImage, newWidth, newHeight);
 
     }
     public static void parse(Image image){
