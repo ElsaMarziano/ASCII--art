f9a5b85ace65342fabe721bd925d17f4f848184f
archsak <aharon.saksonov@mail.huji.ac.il>
2024-02-21 17:21:52 +0200

    imageEditor2

--- image/ImageEditor.java
+++ image/ImageEditor.java
@@ -1,5 +1,9 @@
 package image;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+
 public class ImageEditor {
     /**
      *
@@ -9,6 +13,25 @@ public class ImageEditor {
         //Calculate the correct dimensions of the image
         int newWidth = nearestPowerOf2(image.getWidth());
         int newHeight = nearestPowerOf2(image.getHeight());
+
+        int widthDifference = newWidth - image.getWidth();
+        int heightDifference = newHeight - image.getHeight();
+
+        Color[][] paddedImage;
+        for (int row = 0; row < newHeight; row++){
+            for (int col = 0; col < newWidth; col++){
+                if (row < heightDifference / 2 || row >= newHeight - heightDifference / 2) {
+                    paddedImage[row][col] = Color.WHITE;
+                }
+                else if (col < widthDifference / 2 || row >= newWidth - widthDifference / 2) {
+                    paddedImage[row][col] = Color.WHITE;
+                }
+                else{
+                    paddedImage[row][col] =
+                }
+            }
+        }
+        image = new Image()
 
     }
     public static void parse(Image image){
